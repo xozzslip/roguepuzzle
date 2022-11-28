4fbaf460d3b797cebb22c48bc84e3029b7b3e3c1
xozzslip <xozzslip@gmail.com>
2022-11-28 17:38:23 +0700

    implement entity resizing

--- win_entry.cpp
+++ win_entry.cpp
@@ -4,18 +4,99 @@
 #include <stdio.h>
 #include <strsafe.h>
 
+
+typedef struct {
+    char name[MAX_PATH];
+    int width;
+    int height;
+    uint32_t* pixels;
+} Image;
+
+typedef struct {
+    int id;
+    int x;
+    int y;
+    int scaledWidth; // scaling goes first
+    int scaledHeight;
+    float rotate; // then rotate the image
+    int width; // resulting size of scaled and rotated
+    int height;
+    Image *image;
+    uint32_t* pixels; // cache for scaled and rotated image
+} Entity;
+
 static uint32_t* BitmapMemory;
 static BITMAPINFO BitmapInfo;
 static int WindowHeight;
 static int WindowWidth;
 static bool Running = true;
+static const int MAX_ENTITIES = 1000;
+static Entity entities[MAX_ENTITIES];
+static int entitiesCount;
+static int entityId;
+static const int MAX_IMAGES = 1000;
+static int imagesCount;
+static Image images[MAX_IMAGES];
 
-typedef struct {
-    char* name;
-    int width;
-    int height;
-    uint32_t* pixels;
-} Image;
+
+bool StringEqualTo(char* s, const char* sample);
+
+Entity* CreateEntity(const char* bmpName) {
+    bool found = false;
+    Image* image = NULL;
+    for (int i = 0; i < imagesCount; i++) {
+        image = &images[i];
+        if (StringEqualTo(image->name, bmpName)) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        return NULL;
+    }
+    Entity* entity = &entities[entitiesCount];
+    entity->image = image;
+    entity->id = entityId;
+    entity->scaledWidth = image->width;
+    entity->scaledHeight= image->height;
+    entity->rotate = 0;
+    entity->width = image->width;
+    entity->height = image->height;
+    entity->x = 0;
+    entity->y = 0;
+    entity->pixels = (uint32_t*)VirtualAlloc(0, image->width * image->height * 4, MEM_COMMIT, PAGE_READWRITE);
+    for (int i = 0; i < image->width * image->height; i++) {
+        entity->pixels[i] = image->pixels[i];
+    }
+    entityId++;
+    entitiesCount++;
+    return entity;
+}
+
+void MoveEntity(Entity* entity, int x, int y) {
+    entity->x = x;
+    entity->y = y;
+}
+
+void ResizeEntity(Entity* entity, int newWidth, int newHeight) {
+    entity->pixels = (uint32_t*)VirtualAlloc(0, newWidth * newHeight * 4, MEM_COMMIT, PAGE_READWRITE);
+    entity->width = newWidth;
+    entity->height = newHeight;
+    int originWidth = entity->image->width;
+    int originHeight = entity->image->height;
+    float scaleX = float(newWidth) / float(originWidth);
+    float scaleY = float(newHeight) / float(originHeight);
+    for (int i = 0; i < newWidth * newHeight; i++) {
+        int newX = i % newWidth;
+        int newY = i / newWidth;
+        int originX = int(float(newX) / scaleX);
+        int originY = int(float(newY) / scaleY);
+        int originI = originX + originY * originWidth;
+        entity->pixels[i] = entity->image->pixels[originI];
+    }
+
+}
+
 
 void DebugLog(const char* format, ...) {
     char s[256];
@@ -41,92 +122,108 @@ bool StringEqualTo(char* s, const char* sample) {
         if (s[i] != sample[i]) {
             return false;
         }
-        i += 1;
+i += 1;
     }
     return true;
 }
 
-void FatalError(const char *format, ...) {
+void FatalError(const char* format, ...) {
     LPVOID lpMsgBuf;
-    DWORD dw = GetLastError(); 
+    DWORD dw = GetLastError();
     FormatMessage(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
         FORMAT_MESSAGE_FROM_SYSTEM |
         FORMAT_MESSAGE_IGNORE_INSERTS,
         NULL,
         dw,
         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPTSTR) &lpMsgBuf,
-        0, NULL );
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
 
     char s[256];
     va_list argptr;
     va_start(argptr, format);
-	StringCchVPrintfA(s,
-			256,
-			format,
-			argptr);
+    StringCchVPrintfA(s,
+        256,
+        format,
+        argptr);
 
-	va_end(argptr);
+    va_end(argptr);
     DebugLog("%s: %s", s, lpMsgBuf);
-	ExitProcess(1);
+    ExitProcess(1);
 }
 
 
-void ParseBMP(uint8_t* bytes, int bytesCount, Image* image) {
-	if (!StringEqualTo((char*)bytes, "BM")) {
-		FatalError("invalid bmp header\n");
-	}
-	int headerSize = *((int32_t*)(bytes + 14));
-	int bitsPerPixel = *((int16_t*)(bytes + 28));
-	int bmpSize = *((int32_t*)(bytes + 2));
-	int pixelsOffset = *((int32_t*)(bytes + 10));
-	int width = *((int32_t*)(bytes + 18));
-	int height = *((int32_t*)(bytes + 22));
-	int compression = *((int32_t*)(bytes + 30));
-	if (bytesCount!= bmpSize) {
-		FatalError("size of bitmap on the disk and in header are not equal\n");
-	}
-	if (headerSize != 40) {
-		FatalError("unsupported bmp header format, now only BITMAPINFOHEADER is supported, received %d bytes header insted of 40\n", headerSize);
-	}
-	if (pixelsOffset != headerSize + 14) {
-		FatalError("bmp has invalid pixelsOffsset and headerSize\n");
-	}
-	if (compression != 0) {
-		FatalError("compressed are not supported\n");
-	}
-	if ((bitsPerPixel % 8) != 0) {
-		FatalError("unsupported bmp format, bits per pixel must be devidible by 8\n");
-	}
-	if (bitsPerPixel != 24) {
-		FatalError("unsupported bmp format, only 24 bits per pixel are available\n");
-	}
-	int bytesPerPixel = bitsPerPixel / 8;
-	uint32_t *pixels = (uint32_t *)VirtualAlloc(0, width * height * 4, MEM_COMMIT, PAGE_READWRITE);
+void UnpackBitmapBytes(uint8_t* bytes, int bytesCount, Image* image) {
+    if (!StringEqualTo((char*)bytes, "BM")) {
+        FatalError("invalid bmp header\n");
+    }
+
+    BITMAPFILEHEADER* header = (BITMAPFILEHEADER*)bytes;
+    int bmpSize = header->bfSize;
+    int pixelsOffset = header->bfOffBits;
+    int headerSize = *((int32_t*)(bytes + 14));
+    int bitsPerPixel;
+    int width;
+    int height;
+    int compression;
+    int i = BI_JPEG;
+    if (headerSize == 124) {
+        BITMAPV5HEADER* info = (BITMAPV5HEADER*)(bytes + sizeof(BITMAPFILEHEADER));
+        bitsPerPixel = info->bV5BitCount;
+        width = info->bV5Width;
+        height = info->bV5Height;
+        compression = info->bV5Compression;
+    }
+    else if (headerSize == 40) {
+        BITMAPINFOHEADER* info = (BITMAPINFOHEADER*)(bytes + sizeof(BITMAPFILEHEADER));
+        bitsPerPixel = info->biBitCount;
+        width = info->biWidth;
+        height = info->biHeight;
+        compression = info->biCompression;
+    }
+    else {
+        FatalError("unsupported bmp header format, now only BITMAPINFOHEADER/BITMAPV5HEADER is supported, but received %d bytes header\n");
+    }
+    if (bytesCount != bmpSize) {
+        FatalError("size of bitmap on the disk and in header are not equal\n");
+    }
+    if (pixelsOffset != headerSize + 14) {
+        FatalError("bmp has invalid pixelsOffsset and headerSize\n");
+    }
+    if ((bitsPerPixel % 8) != 0) {
+        FatalError("unsupported bmp format, bits per pixel must be devidible by 8\n");
+    }
+    if (bitsPerPixel != 24 && bitsPerPixel != 32) {
+        FatalError("unsupported bmp format, only 24/32 bits per pixel are available\n");
+    }
+    int bytesPerPixel = bitsPerPixel / 8;
+    uint32_t* pixels = (uint32_t*)VirtualAlloc(0, width * height * 4, MEM_COMMIT, PAGE_READWRITE);
     if (pixels == NULL) {
         FatalError("failed to allocate array for bmp pixels\n");
     }
-	int oneRowSize = bytesPerPixel * width;
-	int allignedRowSize = oneRowSize;
-	if (oneRowSize % 4 != 0) {
-		allignedRowSize = ((oneRowSize / 4) + 1) * 4;
-	}
-	if (allignedRowSize * height + pixelsOffset != bytesCount) {
-		FatalError("bmp file size is not equal to estimated header size + pixels size\n");
-	}
-	// pixels stored bottom to top
+    int oneRowSize = bytesPerPixel * width;
+    int allignedRowSize = oneRowSize;
+    if (oneRowSize % 4 != 0) {
+        allignedRowSize = ((oneRowSize / 4) + 1) * 4;
+    }
+    if (allignedRowSize * height + pixelsOffset != bytesCount) {
+        FatalError("bmp file size is not equal to estimated header size + pixels size\n");
+    }
+    // pixels stored bottom to top
     for (int i = 0; i < height; i++) {
-		int offset = pixelsOffset + allignedRowSize * (height - i - 1);
-		for (int j = 0; j < width; j++) {
+        int offset = pixelsOffset + allignedRowSize * (height - i - 1);
+        for (int j = 0; j < width; j++) {
             uint32_t pixel = *(uint32_t*)(bytes + offset + j * bytesPerPixel);
-            
-			uint8_t blue = *((uint8_t *)&pixel);
-			uint8_t green = *(((uint8_t *)&pixel) + 1);
-			uint8_t red = *(((uint8_t *)&pixel) + 2);
-            uint8_t unused = *(((uint8_t*)&pixel) + 3);
-            pixel = pixel & 0x00ffffff;
-            // DebugLog("pixel x=%d y=%d p=%x b=%x g=%x r=%x u=%x\n", j, i, pixel, blue, green, red, unused);
+            uint8_t blue = *((uint8_t*)&pixel);
+            uint8_t green = *(((uint8_t*)&pixel) + 1);
+            uint8_t red = *(((uint8_t*)&pixel) + 2);
+            uint8_t alpha = *(((uint8_t*)&pixel) + 3);
+
+            if (bitsPerPixel == 24) {
+                pixel = pixel & 0x00ffffff;
+            }
+            //DebugLog("pixel x=%d y=%d p=%x b=%x g=%x r=%x a=%x\n", j, i, pixel, blue, green, red, alpha);
             pixels[i * width + j] = pixel;
 		}
 	}
@@ -192,9 +289,6 @@ int WinMain(
     LPSTR     lpCmdLine,
     int       nShowCmd)
 {
-    const int MAX_IMAGES = 1000;
-    int imagesCount = 0;
-    Image images[MAX_IMAGES] = {};
     WIN32_FIND_DATA fileMetadata = {};
     HANDLE dirHandle = FindFirstFile("assets\\*", &fileMetadata);
     if (INVALID_HANDLE_VALUE == dirHandle) {
@@ -219,7 +313,6 @@ int WinMain(
         if (fileSize == 0) {
             FatalError("assets with size=0 bytes are not supported\n");
         }
-        char* fileName = fileMetadata.cFileName;
         HANDLE fileHandle = CreateFile(filePath,               // file to open
             GENERIC_READ,          // open for reading
             FILE_SHARE_READ,       // share for reading
@@ -239,8 +332,9 @@ int WinMain(
             FatalError("failed to fully read asset content\n");
         }
         Image *image = &images[imagesCount];
-        ParseBMP(assetContent, fileSize, image);
-        image->name = fileName;
+        char* fileName = fileMetadata.cFileName;
+        UnpackBitmapBytes(assetContent, fileSize, image);
+        StringCchCopy(image->name, MAX_PATH, fileName);
         imagesCount += 1;
         VirtualFree(assetContent, 0, MEM_RELEASE);
 
@@ -265,7 +359,14 @@ int WinMain(
         FatalError("failed to create a window\n");
     }
     int timeframe = 0;
-    Image image = images[0];
+    if (CreateEntity("character.bmp") == NULL) {
+        FatalError("failed to create character entity\n");
+    }
+    /*
+    if (CreateEntity("curve.bmp") == NULL) {
+        FatalError("failed to create character entity\n");
+    }
+    */
     while (Running) {
         MSG message = {};
         while (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
@@ -278,13 +379,36 @@ int WinMain(
         timeframe++;
 
         for (int i = 0; i < WindowWidth * WindowHeight; i++) {
+            BitmapMemory[i] = 0;
+        }
+        for (int i = 0; i < entitiesCount; i++) {
+            Entity *entity = &entities[i];
+            ResizeEntity(entity, WindowWidth, WindowHeight);
+            for (int innerIndex = 0; innerIndex < entity->width * entity->height; innerIndex++) {
+                int innerX = innerIndex % entity->width;
+                int innerY = innerIndex / entity->width;
+
+                int screenX = entity->x + innerX;
+                int screenY = entity->y + innerY;
+                int screenIndex = screenX + screenY * WindowWidth;
+                BitmapMemory[screenIndex] = entity->pixels[innerIndex];
+            }
+        }
+        
+        /*
+        Image image = images[0];
+        for (int i = 0; i < WindowWidth * WindowHeight; i++) {
             int y = i / WindowWidth; 
-            int x = i - y * WindowWidth + timeframe; 
+            int x = i - y * WindowWidth; 
             int imageX = x % image.width;
             int imageY = y % image.height;
             int imageI = imageX + imageY * image.width;
             BitmapMemory[i] = image.pixels[imageI];
         }
+        */
+
+
+        
 
         StretchDIBits(
             GetDC(hWnd),
